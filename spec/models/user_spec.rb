b2bb4b072d31ba6327bfbe86cc8ed5fcdc18ebc0
nathanallen <thenathanator@gmail.com>
2016-01-25 14:08:37 -0800

    improved specs w/ factory

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,30 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+describe User do
 
-  before do
-        user_params = Hash.new
-        user_params[:first_name] = FFaker::Name.first_name
-        user_params[:last_name] = FFaker::Name.last_name
-        user_params[:email] = FFaker::Internet.email
-        user_params[:password] = FFaker::Lorem.words(2).join
-
-        @user = User.new(user_params)
-        @user.save
-        p @user.inspect
-  end
-
+  let(:user) { FactoryGirl.build(:user) }
 
   describe User do
     it { should validate_presence_of(:first_name) }
+    it { should validate_length_of(:first_name).is_at_most(255) }
+
     it { should validate_presence_of(:last_name) }
-    it { should validate_presence_of(:email) }
+    it { should validate_length_of(:last_name).is_at_most(255) }
+
+
     it { should validate_presence_of(:password) }
-    it do
-                 should ensure_length_of(:password).
-                   is_at_least(8)
-               end
-    it { should validate_uniqueness_of(:email) }
+    it { should ensure_length_of(:password).is_at_least(8) }
+    it { should ensure_length_of(:password).is_at_most(72) }
     it { should have_secure_password }
+
+    it { should validate_presence_of(:email) }
+    it { should validate_length_of(:email).is_at_most(255) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+
+    context "a valid email" do
+      it "should contain an @ symbol" do
+        user.email = "asdf"
+        expect{spot.save!}.to raise_error
+      end
+    end
   end
 end
